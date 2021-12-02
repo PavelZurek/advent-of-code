239210eadb1c0a2a0e4f2e8f55816610932ed9c0
Pavel Žůrek <PavelZrk@gmail.com>
2021-12-02 13:38:58 +0100

    Day 02 Part 2

--- 2021/02/main.py
+++ 2021/02/main.py
@@ -29,6 +29,30 @@ first()
 # Second part
 
 def second():
-	pass
+	aim = 0
+	horizontal = 0
+	depth = 0
+
+	file = open("data.txt", "r")
+
+	for line in file:
+		if line == "" or line == '\n':
+			break
+
+		parts = line.strip().split(' ')
+		
+		command = parts[0]
+		distance = int(parts[1])
+
+
+		if command == 'down':
+			aim = aim + distance
+		elif command == 'up':
+			aim = aim - distance
+		elif command == 'forward':
+			horizontal = horizontal + distance
+			depth = depth + (aim * distance)
+
+	print("Second: {}".format(horizontal * depth))
 
 second()

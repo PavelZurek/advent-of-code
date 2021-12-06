3b6ab4801e9dc32057b32b2d36977f97880c57c6
Pavel Žůrek <PavelZrk@gmail.com>
2021-12-06 18:45:54 +0100

    Day 05 Part 2

--- 2021/05/main.py
+++ 2021/05/main.py
@@ -1,6 +1,4 @@
-# First part
-
-def first():
+def getPoints():
     x1s, x2s, y1s, y2s = [[],[],[],[]]
 
     with open("data.txt", "r") as file:
@@ -13,30 +11,53 @@ def first():
             x2, y2 = y.split(',')
             x1, x2, y1, y2 = list(map(int, [x1, x2, y1, y2]))
 
-            if x1 == x2 or y1 == y2:
-                x1s.append(min(x1, x2))  
-                y1s.append(min(y1, y2))
-                x2s.append(max(x1, x2))
-                y2s.append(max(y1, y2))
+            x1s.append(x1)  
+            y1s.append(y1)
+            x2s.append(x2)
+            y2s.append(y2)
 
+    return [x1s, x2s, y1s, y2s]
+
+def getDiagram(x1s, x2s, y1s, y2s, countDiagonals=False):
     maxX = max([max(x1s), max(x2s)]) + 1
     maxY = max([max(y1s), max(y2s)]) + 1
     diagram = [list(map(int, list('0'*maxY))) for i in range(maxX)]
 
     for i in range(len(x1s)):
         if x1s[i] == x2s[i]:
-            for y in range(y1s[i], y2s[i] + 1):
+            rng = range(y1s[i], y2s[i] + 1) if y1s[i] < y2s[i] else range(y2s[i], y1s[i] + 1)
+            for y in rng:
                 diagram[y][x1s[i]] += 1
         elif y1s[i] == y2s[i]:
-            for x in range(x1s[i], x2s[i] + 1):
-                diagram[y1s[i]][x] += 1                                      
+            rng = range(x1s[i], x2s[i] + 1) if x1s[i] < x2s[i] else range(x2s[i], x1s[i] + 1)
+            for x in rng:
+                diagram[y1s[i]][x] += 1
+        elif countDiagonals:
+            stepX = 1 if x1s[i] < x2s[i] else -1
+            stepY = 1 if y1s[i] < y2s[i] else -1
 
+            for j in range(abs(x1s[i] - x2s[i]) + 1):
+                diagram[y1s[i] + stepY * j][x1s[i] + stepX * j] += 1
+
+    return diagram
+
+def getResult(diagram):
     result = 0
+
     for dr in diagram:
         for n in dr:
             if n > 1:
                 result += 1
+                
+    return result
 
+# First part
+
+def first():
+    x1s, x2s, y1s, y2s = getPoints()
+    diagram = getDiagram(x1s, x2s, y1s, y2s)
+
+    result = getResult(diagram)
     print("First: {}".format(result))
 
 first()
@@ -44,7 +65,10 @@ first()
 # Second part
 
 def second():
-    result = 0
-    print("Second: {}".format(result))
+    x1s, x2s, y1s, y2s = getPoints()
+    diagram = getDiagram(x1s, x2s, y1s, y2s, True)
 
-#second()
+    result = getResult(diagram)
+    print("Second: {}".format(result)) # Not 950476 (high)
+
+second()

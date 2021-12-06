781c5f2b10f768dacda0f212ba66b93ff212ba2a
Pavel Žůrek <PavelZrk@gmail.com>
2021-12-06 19:00:36 +0100

    Day 06 Part 2

--- 2021/06/main.py
+++ 2021/06/main.py
@@ -22,7 +22,21 @@ first()
 # Second part
 
 def second():
-    result = 0
-    print("Second: {}".format(result)) # Not 950476 (high)
+    data = []
 
-#second()
+    with open("data.txt", "r") as file:
+        data = list(map(int, file.readline().strip().split(',')))
+    
+    counts = [0 for _ in range(9)]
+    for n in data:
+        counts[n] += 1
+
+    for i in range(256):
+        n = counts.pop(0)
+        counts[6] += n
+        counts.append(n)
+
+    result = sum(counts)
+    print("Second: {}".format(result))
+
+second()

52f1acd8b7105692f50395f9caf2df594345e213
Pavel Žůrek <PavelZrk@gmail.com>
2021-12-09 16:56:38 +0100

    Day 09 Part 2

--- 2021/09/main.py
+++ 2021/09/main.py
@@ -1,8 +1,7 @@
-# First part
+neighbour_coords = [[-1, 0], [0, -1], [1, 0], [0, 1]]
 
-def first():
+def getData():
     data = []
-    neighbour_coords = [[-1, 0], [0, -1], [1, 0], [0, 1]]
 
     with open("data.txt", "r") as file:
         for line in file:
@@ -11,7 +10,10 @@ def first():
 
             data.append(line.strip())
 
-    result = 0
+    return data
+
+def getLocalMinsCoords(data):
+    local_mins = []
 
     for i in range(len(data)):
         for j in range(len(data[i])):
@@ -23,9 +25,22 @@ def first():
                 if 0 <= x < len(data) and 0 <= y < len(data[i]):
                     if int(data[i][j]) >= int(data[x][y]):
                         is_local_min = False
-            
+
             if is_local_min:
-                result += int(data[i][j]) + 1
+                local_mins.append([i, j])
+
+    return local_mins
+
+
+# First part
+
+def first():
+    data = getData()
+    local_mins = getLocalMinsCoords(data)
+
+    result = 0
+    for local_min in local_mins:
+        result += int(data[local_min[0]][local_min[1]]) + 1
 
     print("First: {}".format(result))
 
@@ -34,7 +49,34 @@ first()
 # Second part
 
 def second():
-    result = 0
+    data = getData()
+    local_mins = getLocalMinsCoords(data)
+
+    groups = [[local_mins[i]] for i in range(len(local_mins))]
+
+    for i in range(len(groups)):
+        change = True
+        while(change):
+            change = False
+            for j in range(len(groups[i])):
+                coords = groups[i][j]
+
+                for nc in neighbour_coords:
+                    x = coords[0] + nc[0]
+                    y = coords[1] + nc[1]
+
+                    if 0 <= x < len(data) and 0 <= y < len(data[x]):
+                        if data[x][y] != '9':
+                            if [x, y] not in groups[i]:
+                                groups[i].append([x, y])
+                                change = True
+
+    groups.sort(key=len)
+
+    result = 1
+    for i in range(1, 4):
+        result = result * len(groups[len(groups)-i])
+
     print("Second: {}".format(result))
 
-#second()
+second()

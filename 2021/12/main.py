fe1f4611c95386f8474b6021e8a067563609fca6
Pavel Žůrek <PavelZrk@gmail.com>
2021-12-12 15:38:34 +0100

    Day 12 Part 2

--- 2021/12/main.py
+++ 2021/12/main.py
@@ -1,6 +1,4 @@
-# First part
-
-def first():
+def getData():
     data = []
 
     with open("data.txt", "r") as file:
@@ -10,7 +8,11 @@ def first():
 
             data.append(line.strip().split('-'))
 
+    return data
+
+def getStartingPaths(data):
     possible_paths = []
+
     for path in data:
         p = list(path)
         if path[0] == 'start':
@@ -18,6 +20,13 @@ def first():
         elif path[1] == 'start':
             possible_paths.append(list(reversed(p)))
 
+    return possible_paths
+
+# First part
+
+def first():
+    data = getData()
+    possible_paths = getStartingPaths(data)
     result = 0
 
     while len(possible_paths) > 0:
@@ -38,15 +47,61 @@ def first():
             else:
                 possible_paths.append(new_path)
 
-
     print("First: {}".format(result))
 
 first()
 
 # Second part
 
+def isPathValid(path):
+    char_counts = {}
+
+    for char in path:
+        if char not in char_counts.keys():
+            char_counts[char] = 0
+
+        char_counts[char] += 1
+
+    found_two = False
+    for char in char_counts.keys():
+        if (char == 'start' or char == 'end') and char_counts[char] > 1:
+            return False
+        if not char.isupper():
+            if char_counts[char] > 2:
+                return False
+            if char_counts[char] == 2:
+                if found_two:
+                    return False
+                found_two = True
+
+    return True
+
 def second():
+    data = getData()
+    possible_paths = getStartingPaths(data)
     result = 0
+
+    while len(possible_paths) > 0:
+        current_path = possible_paths.pop(0)
+
+        for path in data:
+            new_path = list(current_path)
+
+            if new_path[-1] == path[0]:
+                new_path.append(path[1])
+            elif new_path[-1] == path[1]:
+                new_path.append(path[0])
+            else:
+                continue
+
+            if not isPathValid(new_path):
+                continue
+
+            if new_path[-1] == 'end':
+                result += 1
+            else:
+                possible_paths.append(new_path)
+
     print("Second: {}".format(result))
 
-#second()
+second()

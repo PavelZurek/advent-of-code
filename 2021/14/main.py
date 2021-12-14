2a451bce579b7a440b05615e5d1e57aa67722536
Pavel Žůrek <PavelZrk@gmail.com>
2021-12-14 18:27:11 +0100

    Day 14 Part 2

--- 2021/14/main.py
+++ 2021/14/main.py
@@ -1,5 +1,3 @@
-# First part
-
 def getData():
 
     polymer = []
@@ -29,7 +27,9 @@ def getCounts(polymer):
 
     return counts
 
-def polymerization_step(polymer, instructions):
+# First part
+
+def polymerization_step_part1(polymer, instructions):
     new_polymer = []
 
     for i in range(len(polymer) - 1):
@@ -47,11 +47,10 @@ def first():
     polymer, instructions = getData()
 
     for _ in range(10):
-        polymer = polymerization_step(polymer, instructions)
+        polymer = polymerization_step_part1(polymer, instructions)
 
     counts = getCounts(polymer)
 
-    print(counts)
     result = max(counts.values()) - min(counts.values())
     print("First: {}".format(result))
 
@@ -59,8 +58,50 @@ first()
 
 # Second part
 
+def polymerization_step_part2(pair_counts, instructions):
+    result = {}
+
+    for pair in pair_counts.keys():
+        if pair not in instructions.keys():
+            result[pair] = pair
+        else:
+            new_pair_counts = [pair[0] + instructions[pair], instructions[pair] + pair[1]]
+
+            for new_pair in new_pair_counts:
+                if new_pair in result.keys():
+                    result[new_pair] += pair_counts[pair]
+                else:
+                    result[new_pair] = pair_counts[pair]
+
+    return result
+
 def second():
-    result = 0
+    polymer, instructions = getData()
+    pair_counts = {}
+
+    for i in range(len(polymer) - 1):
+        key = ''.join(polymer[i:i + 2])
+
+        if key not in pair_counts.keys():
+            pair_counts[key] = 1
+        else:
+            pair_counts[key] += 1
+
+    for _ in range(40):
+        pair_counts = polymerization_step_part2(pair_counts, instructions)
+
+    total_counts = {}
+    for pair in pair_counts.keys():
+        char = pair[0]
+
+        if char in total_counts.keys():
+            total_counts[char] += pair_counts[pair]
+        else:
+            total_counts[char] = pair_counts[pair]
+
+    total_counts[polymer.pop()] += 1
+
+    result = max(total_counts.values()) - min(total_counts.values())
     print("Second: {}".format(result))
 
-#second()
+second()

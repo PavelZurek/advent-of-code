dbe51625226565c1ab85bcbf86fcbcd8ddd4f472
Pavel Žůrek <PavelZrk@gmail.com>
2022-12-01 17:02:01 +0100

    2022 Day 1 Part 2

--- 2022/01/main.py
+++ 2022/01/main.py
@@ -22,7 +22,22 @@ first()
 # Second part
 
 def second():
-    result = 0
+    file = open("data.txt", "r")
+
+    current_sum = 0
+    top = [0, 0, 0]
+
+    for line in file:
+        if line == "" or line == '\n':
+            if top[0] < current_sum:
+                top.pop(0)
+                top.append(current_sum)
+                top.sort()
+            current_sum = 0
+        else:
+            current_sum += int(line)
+
+    result = top[0]+top[1]+top[2]
     print("Second: {}".format(result))
 
-#second()
+second()

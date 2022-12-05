dde90c69dace67fde511f3ba415f6a4588910bec
Pavel Žůrek <PavelZrk@gmail.com>
2022-12-05 18:30:09 +0100

    2022 Day 4 Part 2

--- 2022/04/main.py
+++ 2022/04/main.py
@@ -22,8 +22,32 @@ first()
 
 # Second part
 
+def intersect(a, b):
+    result = []
+
+    if (len(a) < len(b)):
+        temp = a
+        a = b
+        b = temp
+
+    for i in range(0, len(a)):
+        if (b.__contains__(a[i])):
+            result.append(a[i])
+
+    return result
+
 def second():
     result = 0
+
+    with open("data.txt", "r") as file:
+        for line in file:
+            [rangeA, rangeB] = line.strip().split(',')
+            [aFrom, aTo] = map(int, rangeA.split('-'))
+            [bFrom, bTo] = map(int, rangeB.split('-'))
+
+            if len(intersect(list(range(aFrom, aTo+1)), list(range(bFrom, bTo+1)))) > 0:
+                result += 1
+
     print("Second: {}".format(result))
 
-#second()
+second()

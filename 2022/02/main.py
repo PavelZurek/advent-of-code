63308490bcfbf157c31a3d71c5212dea19cc5869
Pavel Žůrek <PavelZrk@gmail.com>
2022-12-05 16:21:41 +0100

    2022 Day 2 Part 2

--- 2022/02/main.py
+++ 2022/02/main.py
@@ -1,11 +1,11 @@
-# First part
-
 ROCK = 1
 PAPPER = 2
 SCISSORS = 3
 
-WIN = 6
-DRAW = 3
+WIN_SCORE = 6
+DRAW_SCORE = 3
+
+# First part
 
 def first():
     result = 0
@@ -19,13 +19,13 @@ def first():
             result += my
 
             if (op == my):
-                result += DRAW
+                result += DRAW_SCORE
             if (op == ROCK and my == PAPPER):
-                result += WIN
+                result += WIN_SCORE
             if (op == PAPPER and my == SCISSORS):
-                result += WIN
+                result += WIN_SCORE
             if (op == SCISSORS and my == ROCK):
-                result += WIN
+                result += WIN_SCORE
 
     print("First: {}".format(result))
 
@@ -34,7 +34,36 @@ first()
 # Second part
 
 def second():
+    WIN = 3
+    DRAW = 2
+    LOSS = 1
+
     result = 0
+
+    with open("data.txt", "r") as file:
+        for line in file:
+            [op, r] = line.split()
+            op = ord(op)-64
+            r = ord(r)-87
+
+            if (r == DRAW):
+                result += op + DRAW_SCORE
+            elif (r == WIN):
+                result += WIN_SCORE
+                if (op == ROCK):
+                    result += PAPPER
+                if (op == PAPPER):
+                    result += SCISSORS
+                if (op == SCISSORS):
+                    result += ROCK
+            else:
+                if (op == ROCK):
+                    result += SCISSORS
+                if (op == PAPPER):
+                    result += ROCK
+                if (op == SCISSORS):
+                    result += PAPPER
+
     print("Second: {}".format(result))
 
 second()

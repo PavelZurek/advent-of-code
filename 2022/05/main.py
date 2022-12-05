6fb30c726f369628e64f94301e3fbe7d5f38254f
Pavel Žůrek <PavelZrk@gmail.com>
2022-12-05 17:26:03 +0100

    2022 Day 5 Part 2

--- 2022/05/main.py
+++ 2022/05/main.py
@@ -1,35 +1,46 @@
+def getInitialState(file):
+    stateLines = []
+
+    # save instructions
+    line = file.readline().strip('\n')
+    while line.strip()[0] == '[': # reading starting state
+        stateLines.append(line)
+        line = file.readline().strip('\n')
+
+    # line now contains column numbers
+    lines = line.strip().split("   ")
+
+    # create empty state
+    currentState = {}
+    for lineNumber in lines:
+        currentState[lineNumber] = []
+
+    # fill current state
+    for stateLine in reversed(stateLines):
+        for col in range(0, len(currentState)):
+            cartId = stateLine[col*4+1:col*4+2]
+            if (cartId.strip() != ""):
+                stateKey = "{}".format(col+1)
+                currentState[stateKey].append(cartId)
+
+    # empty row to divide state and instructions
+    file.readline()
+
+    return currentState
+
+def getResult(currentState):
+    result = []
+    for col in currentState:
+        result.append(currentState[col][-1])
+
+    return ''.join(result)
+
 # First part
 
 def first():
     with open("data.txt", "r") as file:
-        stateLines = []
-
-        # save instructions
-        line = file.readline().strip('\n')
-        while line.strip()[0] == '[': # reading starting state
-            stateLines.append(line)
-            line = file.readline().strip('\n')
-
-        # line now contains column numbers
-        lines = line.strip().split("   ")
-
-        # create empty state
-        currentState = {}
-        for lineNumber in lines:
-            currentState[lineNumber] = []
-
-        # fill current state
-        for stateLine in reversed(stateLines):
-            for col in range(0, len(currentState)):
-                cartId = stateLine[col*4+1:col*4+2]
-                if (cartId.strip() != ""):
-                    stateKey = "{}".format(col+1)
-                    currentState[stateKey].append(cartId)
-
-        #print(currentState)
-
-        # empty row to divide state and instructions
-        file.readline()
+        # load the state
+        currentState = getInitialState(file)
 
         # calculate instructions
         line = file.readline().strip(" \n")
@@ -40,22 +51,33 @@ def first():
                 cartId = currentState[f].pop()
                 currentState[t].append(cartId)
 
+            # read next line
             line = file.readline().strip(" \n")
 
-    # calculate result
-    result = []
-
-    for col in currentState:
-        result.append(currentState[col][-1])
-
-    print("First: {}".format(''.join(result)))
+    print("First: {}".format(getResult(currentState)))
 
 first()
 
 # Second part
 
 def second():
-    result = 0
-    print("Second: {}".format(result))
+    with open("data.txt", "r") as file:
+        # load the state
+        currentState = getInitialState(file)
 
-#second()
+        # calculate instructions
+        line = file.readline().strip(" \n")
+        while line != "":
+            # parse instruction
+            [_, x, _, f, _, t] = line.split(' ')
+            cartIds = currentState[f][-int(x):]
+            currentState[f] = currentState[f][0:-int(x)]
+            for cartId in cartIds:
+                currentState[t].append(cartId)
+
+            # read next line
+            line = file.readline().strip(" \n")
+
+    print("Second: {}".format(getResult(currentState)))
+
+second()

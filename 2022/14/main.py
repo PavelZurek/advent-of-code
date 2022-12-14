c6ec3905aebdc04c5bb1091c5d1e68dd1b3b938c
Pavel Žůrek <PavelZrk@gmail.com>
2022-12-14 23:08:55 +0100

    2022 Day 14 Part 2

--- 2022/14/main.py
+++ 2022/14/main.py
@@ -1,28 +1,11 @@
-# First part
-
 EMPTY = '.'
 ROCK = '#'
 SAND = 'O'
 
-def printCave(caveMap):
-    print("---------------------")
-    for j in range(0, 10):
-        for i in range(494, 504):
-            print(caveMap[i][j], end='')
-        print()
-    print("---------------------")
-
-def getTargetCoords(caveMap, currentPoint):
-    possibleMoves = [[0, 1], [-1, 1], [1, 1]]
-
-    for move in possibleMoves:
-        targetCoords = [currentPoint[0]+move[0], currentPoint[1]+move[1]]
-        if caveMap[targetCoords[0]][targetCoords[1]] == EMPTY:
-            return targetCoords
-
-def first():
+def loadCaveMap(bottom=False):
     caveMap = [[EMPTY for j in range(0, 1000)] for i in range(0, 1000)]
     caveMap[500][0] = '+'
+    maxY = 0
 
     with open("data.txt", "r") as file:
         for line in file:
@@ -46,8 +29,45 @@ def first():
                     stepY = 1 if currentPoint[1] < nextPoint[1] else -1
                     for j in range(currentPoint[1], nextPoint[1]+stepY, stepY):
                         caveMap[currentPoint[0]][j] = ROCK
+                        if j > maxY:
+                            maxY = j
 
-    #printCave(caveMap)
+    if bottom:
+        for i in range(0, 1000):
+            caveMap[i][maxY+2] = ROCK
+
+    return caveMap
+
+def getTargetCoords(caveMap, currentPoint):
+    possibleMoves = [[0, 1], [-1, 1], [1, 1]]
+
+    for move in possibleMoves:
+        targetCoords = [currentPoint[0]+move[0], currentPoint[1]+move[1]]
+        if caveMap[targetCoords[0]][targetCoords[1]] == EMPTY:
+            return targetCoords
+
+def getResult(caveMap):
+    result = 0
+
+    for i in range(0, 1000):
+        for j in range(0, 1000):
+            if caveMap[i][j] == SAND:
+                result += 1
+
+    return result
+
+def printCave(caveMap):
+    print("---------------------")
+    for j in range(0, 12):
+        for i in range(494, 504):
+            print(caveMap[i][j], end='')
+        print()
+    print("---------------------")
+
+# First part
+
+def first():
+    caveMap = loadCaveMap()
 
     while True:
         try:
@@ -57,20 +77,12 @@ def first():
             while nextCoords != None:
                 sandCoords = nextCoords
                 nextCoords = getTargetCoords(caveMap, nextCoords)
-                #print(nextCoords)
 
             caveMap[sandCoords[0]][sandCoords[1]] = SAND
-
-            #printCave(caveMap)
         except IndexError:
             break
 
-    result = 0
-    for i in range(0, 1000):
-        for j in range(0, 1000):
-            if caveMap[i][j] == SAND:
-                result += 1
-
+    result = getResult(caveMap)
     print("First: {}".format(result))
 
 first()
@@ -78,7 +90,19 @@ first()
 # Second part
 
 def second():
-    result = 0
+    caveMap = loadCaveMap(True)
+
+    while caveMap[500][0] != SAND:
+        sandCoords = [500, 0]
+
+        nextCoords = getTargetCoords(caveMap, sandCoords)
+        while nextCoords != None:
+            sandCoords = nextCoords
+            nextCoords = getTargetCoords(caveMap, nextCoords)
+
+        caveMap[sandCoords[0]][sandCoords[1]] = SAND
+
+    result = getResult(caveMap)
     print("Second: {}".format(result))
 
-#second()
+second()

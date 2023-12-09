f2c970d2968585159039aab22767a3d420aafdd7
Pavel Žůrek <PavelZrk@gmail.com>
2023-12-09 23:50:43 +0100

    2023 Day 9 Part 2

--- 2023/09/main.py
+++ 2023/09/main.py
@@ -1,19 +1,22 @@
-# First part
-
 def getDifferenceMap(values):
     return [values[x+1] - values[x] for x in range(0, len(values)-1)]
+
+def getHistory(line):
+    values = [[int(x) for x in line.strip().split(' ')]]
+
+    while True:
+        values.append(getDifferenceMap(values[-1]))
+        if ''.join([str(x) for x in set(values[-1])]) == '0':
+            return values
+
+# First part
 
 def first():
     result = 0
 
     with open("data.txt", "r") as file:
         for line in file:
-            history = [[int(x) for x in line.strip().split(' ')]]
-
-            while True:
-                history.append(getDifferenceMap(history[-1]))
-                if ''.join([str(x) for x in set(history[-1])]) == '0':
-                    break
+            history = getHistory(line)
 
             history[-1].append(0)
             for i in reversed(range(1, len(history))):
@@ -30,6 +33,18 @@ first()
 
 def second():
     result = 0
+
+    with open("data.txt", "r") as file:
+        for line in file:
+            history = getHistory(line)
+
+            history[-1].insert(0, 0)
+            for i in reversed(range(1, len(history))):
+                newValue = history[i-1][0] - history[i][0]
+                history[i-1].insert(0, newValue)
+
+            result += history[0][0]
+
     print("Second: {}".format(result))
 
-#second()
+second()

6c01b3e8571b0f633ec853a91c7817bf6f24dc5b
Pavel Žůrek <PavelZrk@gmail.com>
2023-12-11 19:38:00 +0100

    2023 Day 11 Part 2

--- 2023/11/main.py
+++ 2023/11/main.py
@@ -1,6 +1,4 @@
-# First part
-
-def first():
+def getGalaxyDistances(expansionCoefficient = 1):
     galaxies = []
     maxCoords = [0, 0]
 
@@ -25,18 +23,13 @@ def first():
         if i in iSpaces: iSpaces.remove(i)
         if j in jSpaces: jSpaces.remove(j)
 
-    maxCoords = [
-        maxCoords[0] + len(iSpaces) + 1,
-        maxCoords[1] + len(jSpaces) + 1
-    ]
-
     for g in galaxies:
         for iSpace in reversed(iSpaces):
             if g[0] > iSpace:
-                g[0] += 1
+                g[0] += expansionCoefficient
         for jSpace in reversed(jSpaces):
             if g[1] > jSpace:
-                g[1] += 1
+                g[1] += expansionCoefficient
 
     distances = []
     for g1i in range(0, len(galaxies)):
@@ -46,6 +39,12 @@ def first():
             distance = abs(g1[0] - g2[0]) + abs(g1[1] - g2[1])
             distances.append(distance)
 
+    return distances
+
+# First part
+
+def first():
+    distances = getGalaxyDistances()
     result = sum(distances)
     print("First: {}".format(result))
 
@@ -54,7 +53,8 @@ first()
 # Second part
 
 def second():
-    result = 0
+    distances = getGalaxyDistances(999999)
+    result = sum(distances)
     print("Second: {}".format(result))
 
-#second()
+second()

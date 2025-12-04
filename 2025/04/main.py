494e2aafcb9c27bfc54a1eebe9f54c968fa29fa4
Pavel Žůrek <PavelZrk@gmail.com>
2025-12-04 08:53:29 +0100

    2025 Day 4 Part 2

--- 2025/04/main.py
+++ 2025/04/main.py
@@ -1,4 +1,11 @@
-# First part
+def loadMap():
+    rolls_map = []
+
+    with open("data.txt", "r") as file:
+        for line in file:
+            rolls_map.append(list(line.strip()))
+
+    return rolls_map
 
 def getRollsCount(rolls_map, i, j):
     count = 0
@@ -8,17 +15,15 @@ def getRollsCount(rolls_map, i, j):
                 count += 1
     return count
 
+# First part
+
 def first():
     result = 0
-    rolls_map = []
-
-    with open("data.txt", "r") as file:
-        for line in file:
-            rolls_map.append(line.strip())
+    rolls_map = loadMap()
 
     for i in range(len(rolls_map)):
         for j in range(len(rolls_map[i])):
-            if rolls_map[i][j] == "@":
+            if rolls_map[i][j] == '@':
                 if getRollsCount(rolls_map, i, j) < 5:
                     result += 1
 
@@ -30,6 +35,19 @@ first()
 
 def second():
     result = 0
+    change = True
+    rolls_map = loadMap()
+
+    while change:
+        change = False
+        for i in range(len(rolls_map)):
+            for j in range(len(rolls_map[i])):
+                if rolls_map[i][j] == "@":
+                    if getRollsCount(rolls_map, i, j) < 5:
+                        result += 1
+                        rolls_map[i][j] = '.'
+                        change = True
+
     print("Second: {}".format(result))
 
-#second()
+second()

b361ec0b10409242884910c5b15154b4dc601215
Pavel Žůrek <PavelZrk@gmail.com>
2025-12-05 13:17:19 +0100

    2025 Day 5 Part 2

--- 2025/05/main.py
+++ 2025/05/main.py
@@ -22,7 +22,35 @@ first()
 # Second part
 
 def second():
+    ranges = []
+
+    with open("data.txt", "r") as file:
+        for line in file:
+            line = line.strip()
+            if "-" in line:
+                new_range = list(map(int, line.split("-")))
+
+                overlaping_ranges = []
+                for range in ranges:
+                    if not (new_range[1] < range[0] or new_range[0] > range[1]):
+                        overlaping_ranges.append(range)
+
+                if overlaping_ranges:
+                    for range in overlaping_ranges:
+                        ranges.remove(range)
+                        if new_range[0] > range[0]:
+                            new_range[0] = range[0]
+                        if new_range[1] < range[1]:
+                            new_range[1] = range[1]
+
+                ranges.append(new_range)
+            else:
+                break
+
     result = 0
+    for range in ranges:
+        result += range[1] - range[0] + 1
+
     print("Second: {}".format(result))
 
-#second()
+second()

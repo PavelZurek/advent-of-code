58bff6e31546fa055e0f8116a2130ca170a54d0a
Pavel Žůrek <PavelZrk@gmail.com>
2025-12-06 11:33:22 +0100

    2025 Day 6 Part 2

--- 2025/06/main.py
+++ 2025/06/main.py
@@ -33,6 +33,41 @@ first()
 
 def second():
     result = 0
+    numberlines = []
+    operationline = ""
+
+    with open("data.txt", "r") as file:
+        for line in file:
+            if line.strip().replace(" ", "").isnumeric():
+                numberlines.append(line.replace("\n", ""))
+            else:
+                operationline = line.replace("\n", "")
+
+    numbers = []
+    for c in range(len(numberlines[0])+1):
+        if c < len(operationline) and operationline[c].strip():
+            operation = operationline[c]
+
+        number = ["" for _ in numberlines]
+        for i in range(len(numberlines)):
+            if c < len(numberlines[i]):
+                number[i] += numberlines[i][c]
+
+        number = "".join(number)
+
+        if number.strip().isnumeric():
+            numbers.append(int(number.strip()))
+        else:
+            col_result = numbers[0]
+            if operation == "*":
+                for i in range(1, len(numbers)):
+                    col_result *= numbers[i]
+            elif operation == "+":
+                for i in range(1, len(numbers)):
+                    col_result += numbers[i]
+            result += col_result
+            numbers = []
+
     print("Second: {}".format(result))
 
-#second()
+second()
